9e18f0a397f34d5a66bb8f49712fb64c810fca61
Michal Zmuda <zmudamichal92@gmail.com>
2013-03-07 18:51:36 +0100

    try

--- arytmetyka/ciag.c
+++ arytmetyka/ciag.c
@@ -1,6 +1,29 @@
-#import <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(){
-    printf("YYY");
+void work(int steps){
+    float xf = 0.01f;
+    float tmpf;
+    double xd = 0.01;
+    double tmpd;
+    while(steps--){
+
+    }
+}
+
+
+int main(int argc, char *argv[]){
+    int steps = -1;
+    if(argc<2){
+        printf("\nNie podano liczby poczatkowych wyrazow\n");
+        return 1;
+    } else {
+        steps = atoi(argv[1]);
+        if(steps<1){
+            printf("\nLiczba wyrazow musi byc niemniejza niz 1\n");
+            return 2;
+        }
+    }
+
     return 0;
 }
