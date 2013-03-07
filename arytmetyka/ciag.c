e0ec109a6030ce0f9a4c4e0a759f9146fe111fe2
Michal Zmuda <zmudamichal92@gmail.com>
2013-03-07 20:07:03 +0100

    plot

--- arytmetyka/ciag.c
+++ arytmetyka/ciag.c
@@ -1,13 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+ x{n+1}= x{n} + 3.0 * x{n} (1 - x{n})
+ w tym ciągu błąd reprezentacji jest wzmacniany z każdą iteracją (rekurencja)
+ dodatkowo jest źle uwarunkowany dla x->0 albo x->4/3
+ no a dla x->1 mamy blad anulowania
+*/
+
 void work(int steps){
     float xf = 0.01f;
     float tmpf;
     double xd = 0.01;
     double tmpd;
-    printf("\n\t:double:\t:float:\n\n");
-    while(steps--){
+    //printf("\n\t:double:\t:float:\n\n");
+    FILE* out = fopen("out.tmp","w");
+    fprintf(out,"%f\t%i\t%lf\n",xf,0,xd);
+    int i=0;
+    while(++i<steps){
         tmpf=1-xf;
         tmpf*=xf;
         tmpf*=3.0f;
@@ -18,8 +28,10 @@ void work(int steps){
         tmpd*=3.0;
         xd+=tmpd;
 
-        printf("\t%f\t%lf\n",xf,xd);
+        fprintf(out,"%f\t%i\t%lf\n",xf,i,xd);
+        //printf("\t%f\t%lf\n",xf,xd);
     }
+    fclose(out);
 }
 
 
@@ -38,3 +50,4 @@ int main(int argc, char *argv[]){
     work(steps);
     return 0;
 }
+
